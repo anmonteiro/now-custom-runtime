5ca48b082f23ae5de3641518baa5a235320bdf03
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-03-09 17:18:55 -0800

    delete unneeded function

--- index.ts
+++ index.ts
@@ -1,20 +1,9 @@
 import {Lambda, rename} from '@vercel/build-utils';
-import crypto from 'crypto';
-import objectHash from 'object-hash';
 import type {BuildOptions} from '@vercel/build-utils';
 
-exports.version = 3;
+export const version = 3;
 
-exports.analyze = ({files, entrypoint, config}: BuildOptions) => {
-  // @ts-expect-error
-  const entrypointHash = files[entrypoint].digest;
-  const objHash = objectHash(config, {algorithm : 'sha1'});
-  const combinedHashes = [ entrypointHash, objHash ].join('');
-
-  return crypto.createHash('sha256').update(combinedHashes).digest('hex');
-};
-
-exports.build = async ({files, entrypoint}: BuildOptions) => {
+export function build({files, entrypoint}: BuildOptions) {
   // Set the executable bit for the entrypoint. Somewhere around Now
   // v16.{2,3}.x they stopped keeping the permissions for files uploaded via
   // the CLI on deployments.
